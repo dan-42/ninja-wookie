e7354f452574c85521159188da65ddcd829716fa
schoko <schoko@mina.local>
2015-05-12 21:45:51 +0200

    FEATURE: first lisener for udp packages

--- src/main.cpp
+++ src/main.cpp
@@ -30,25 +30,73 @@
 #include <bacnet/bacnet.hpp>
 
 
+namespace bacnet { namespace  bvll {
+
+const uint16_t DEFAULT_PORT = 0xBAC0;
+
+class controller {
+
+public:
+
+	controller(boost::asio::io_service &ios) : io_service_(ios), socket_(ios) {
+
+		auto multicast_address = boost::asio::ip::address::from_string("255.255.255.255");
+
+		boost::asio::ip::udp::endpoint multicast_endpoint(multicast_address	, DEFAULT_PORT);
+
+
+		boost::asio::ip::udp::endpoint listen_endpoint(boost::asio::ip::address::from_string("0.0.0.0")	, DEFAULT_PORT);
+		socket_.open(listen_endpoint.protocol());
+		socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+		socket_.bind(listen_endpoint);
+
+		//socket_.set_option(boost::asio::ip::multicast::join_group(multicast_address));
+
+
+		socket_.async_receive_from(
+				boost::asio::buffer(data_, max_length), sender_endpoint_,
+					boost::bind(&controller::handle_receive_from, this,  boost::asio::placeholders::error,  boost::asio::placeholders::bytes_transferred));
+
+	}
+
+
+	void handle_receive_from(const boost::system::error_code& error,   size_t bytes_recvd)	  {
+	    if (!error)
+	    {
+	      std::cout.write(data_, bytes_recvd);
+	      std::cout << std::endl;
+
+	      socket_.async_receive_from(
+	          boost::asio::buffer(data_, max_length), sender_endpoint_,
+	          boost::bind(&controller::handle_receive_from, this,
+	            boost::asio::placeholders::error,
+	            boost::asio::placeholders::bytes_transferred));
+	    }
+	  }
+
+
+private:
+
+	boost::asio::io_service &io_service_;
+	boost::asio::ip::udp::socket socket_;
+	boost::asio::ip::udp::endpoint sender_endpoint_;
+	enum { max_length = std::numeric_limits<uint16_t>::max()};
+	char data_[max_length];
+};
+
+
+}}
+
+
 
 int main(int argc, char* argv[])
 {
   try
   {
-    if (argc != 3)
-    {
-      std::cerr << "Usage: receiver <listen_address> <multicast_address>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    receiver 0.0.0.0 239.255.0.1\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    receiver 0::0 ff31::8000:1234\n";
-      return 1;
-    }
 
     boost::asio::io_service io_service;
-    receiver r(io_service,
-        boost::asio::ip::address::from_string(argv[1]),
-        boost::asio::ip::address::from_string(argv[2]));
+    bacnet::bvll::controller controller(io_service);
+
     io_service.run();
   }
   catch (std::exception& e)
