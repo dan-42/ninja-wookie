b0c7219426c79ad4017b347653b1b8afb968764f
daniel friedrich <dan@fluffybytes.de>
2016-05-19 19:34:46 +0200

    FEATURE: generating property types yeah

--- src/bacnet/type/detail/properties_impl.hpp
+++ src/bacnet/type/detail/properties_impl.hpp
@@ -12,7 +12,9 @@
 #include <string>
 #include <type_traits>
 
-#include <boost/preprocessor.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/punctuation/comma.hpp>
+#include <boost/preprocessor/facilities/expand.hpp>
 #include <boost/mpl/vector.hpp>
 
 
@@ -28,7 +30,7 @@ namespace bacnet { namespace type { namespace property {
 
   template<typename T>  struct is_standard_property : std::true_type { };
 
-  template<typename T>  struct is_special_operation_property : std::false_type { };
+  template<typename T>  struct is_special_property : std::false_type { };
 
 
 // must be implemented for each
@@ -39,6 +41,10 @@ namespace bacnet { namespace type { namespace property {
 
 }}}
 
+template<bacnet::type::property_type T>
+using integral_constant_ = std::integral_constant<bacnet::type::property_type, T>;
+
+#define COMMA ,
 
 #define WOOKIE_BACNET_IS_STANDARD_PROPERTY 1
 #define WOOKIE_BACNET_IS_PROPRIETARY_PROPERTY 0
@@ -49,35 +55,50 @@ namespace bacnet { namespace type { namespace property {
 
 
 #define WOOKIE_CREATE_TRAIT_IS_STD_PROP_TRUE_TYPE(property_name) \
-    template<> struct is_standard_property<property_name> : std::true_type { };   \
+    template<> struct is_standard_property<property_name> : std::true_type { };
 
 #define WOOKIE_CREATE_TRAIT_IS_STD_PROP_FALSE_TYPE(property_name) \
-    template<> struct is_standard_property<property_name> : std::false_type { };    \
+    template<> struct is_standard_property<property_name> : std::false_type { };
 
 
 
 
 #define WOOKIE_CREATE_TRAIT_IS_NORMAL_PROP_TRUE_TYPE(property_name) \
-    template<> struct is_special_operation_property<property_name> : std::true_type { };   \
+    template<> struct is_special_property<property_name> : std::true_type { };
 
 #define WOOKIE_CREATE_TRAIT_IS_NORMAL_PROP_FALSE_TYPE(property_name) \
-    template<> struct is_special_operation_property<property_name> : std::false_type { };    \
+    template<> struct is_special_property<property_name> : std::false_type { };
 
 
+
+#define WOOKIE_DEFINE_BACNET_PROPERTY_SIMPLE_ARGS(property_id)       \
+    bacnet::type::property_type , property_id
 
 #define WOOKIE_DEFINE_BACNET_PROPERTY_SIMPLE(property_name, property_id)       \
-   typedef std::integral_constant<bacnet::type::property_type, property_id>    \
-            property_name;                                                     \
+   typedef integral_constant_<property_id>   \
+   property_name;                                                                            \
     template<>                                                                 \
     struct name<property_name> {                                               \
       static constexpr const char* const value = #property_name;               \
     };                                                                         \
                                                                                \
 
+#define my_args(id) int COMMA id
+
+//#define consttant_(id) typedef std::integral_constant< my_args(id)>
+#define consttant_(id) typedef integral_constant_< id >
+
+#define WOOKIE_DEFINE_BACNET_PROPERTY_SIMPLE_1(property_name, property_id)         \
+    consttant_(property_id)   property_name;                                    \
+    template<>                                                                 \
+    struct name<property_name> {                                               \
+      static constexpr const char* const value = #property_name;               \
+    };                                                                         \
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            \
 
 
 #define WOOKIE_DEFINE_BACNET_PROPERTY_ALL(property_name, property_id, is_standard, is_normal) \
-    WOOKIE_DEFINE_BACNET_PROPERTY_SIMPLE(property_name, property_id)           \
+    WOOKIE_DEFINE_BACNET_PROPERTY_SIMPLE_1(property_name, property_id)           \
                                                                                \
                                                                                \
     BOOST_PP_IF(is_standard,                                                   \
@@ -90,49 +111,30 @@ namespace bacnet { namespace type { namespace property {
     )                                                                          \
 
 
-/*
-#define EACH_PROP_TUPLE_SIMPLE(property_entry)                                 \
-  WOOKIE_DEFINE_BACNET_PROPERTY_SIMPLE(                                        \
-    BOOST_PP_TUPLE_ELEM(0, property_entry),                                    \
-    BOOST_PP_TUPLE_ELEM(1, property_entry),                                    \
-  )
-*/
-
-#define EACH_PROP_TUPLE_ALL(property_entry)                                    \
-  WOOKIE_DEFINE_BACNET_PROPERTY_ALL(                                           \
-    BOOST_PP_TUPLE_ELEM(0, property_entry),                                    \
-    BOOST_PP_TUPLE_ELEM(1, property_entry),                                    \
-    BOOST_PP_TUPLE_ELEM(2, property_entry),                                    \
-    BOOST_PP_TUPLE_ELEM(3, property_entry)                                     \
- )
-
-#define EACH_PROP_TUPLE(R, unused, property_entry)                             \
-  EACH_PROP_TUPLE_ALL(property_entry)
 
 /*
-#define EACH_PROP_TUPLE(R, unused, property_entry)                             \
-    BOOST_PP_IF(                                                               \
-      BOOST_PP_EQUAL(                                                          \
-        2,                                                                     \
-        BOOST_PP_TUPLE_SIZE(property_entry)                                    \
-      ),                                                                       \
-      EACH_PROP_TUPLE_SIMPLE(property_entry),                                  \
-      EACH_PROP_TUPLE_ALL(property_entry)                                      \
-    )                                                                          \
-*/
+ * Many thanks to damien buhl (https://github.com/daminetreg)
+ * who provided me with this awesome macro solution
+ */
 
-#define EXTRACT_PROPERTY_NAME_WITH_COMMA(R, unused, property_entry)            \
-  BOOST_PP_TUPLE_ELEM(0, property_entry),
+#define NM_BACNET_FILLER_0(X)                                                 \
+   WOOKIE_DEFINE_BACNET_PROPERTY_ALL X                                                    \
+    NM_BACNET_FILLER_1
 
-#define EXTRACT_PROPERTY_NAME(R, unused, property_entry)                       \
-  BOOST_PP_TUPLE_ELEM(0, property_entry)
+#define NM_BACNET_FILLER_1(X)                                                  \
+    WOOKIE_DEFINE_BACNET_PROPERTY_ALL X                                                    \
+    NM_BACNET_FILLER_0
+
+#define NM_BACNET_FILLER_0_END
+#define NM_BACNET_FILLER_1_END
+
+#define WOOKIE_DEFINE_BACNET_PROPERTIES(elems)                                   \
+            BOOST_PP_CAT( NM_BACNET_FILLER_0 elems ,_END)                        \
 
 
-#define WOOKIE_DEFINE_BACNET_PROPERTIES(property_seq)                          \
-    BOOST_PP_SEQ_FOR_EACH(  EACH_PROP_TUPLE,                                   \
-                            unused,                                            \
-                            property_seq  )                                    \
-                                                                               \
+
+
+
 /*  typedef boost::mpl::vector<                                                  \
 
       BOOST_PP_SEQ_FOR_EACH(  EXTRACT_PROPERTY_NAME_WITH_COMMA,                \
